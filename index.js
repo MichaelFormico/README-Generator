2c68e28aba2328b13cc5a427fd07a88e087caa8a
Furly <michaelformico@gmail.com>
2023-04-21 20:38:21 -0700

    Updated!

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+
 // TODO: Create an array of questions for user input
 const questions = [
     'What was your motivation?',
@@ -10,6 +12,11 @@ const questions = [
     'What makes your project stand out?',
     'What challenges did you face?',
     'What are your future plans?',
+    'What is your title?',
+    'Who are all the contributors?',
+    'What is this app used for?',
+    'What is your email address?',
+    'What is the link to your GitHub profile?'
 ];
 
 // TODO: Create a function to write README file
@@ -57,16 +64,57 @@ function init() {
             message: questions[6],
             name: 'future',
         },
+        {
+            type:'input',
+            message: questions[7],
+            name: 'title'
+        } ,
+        {
+            type:'input',
+            message: questions[8],
+            name: 'contribution'
+        },
+        {
+            type:'input',
+            message: questions[9],
+            name: 'usage'
+        },
+        {
+            type:'input',
+            message: questions[10],
+            name: 'email'
+        },
+        {
+            type:'input',
+            message: questions[11],
+            name: 'github'
+        },
+        {
+            type:'checkbox',
+            name: 'licenseChoice',
+            message: 'Select any licenses you would like, or none.',
+            choices: [
+              { name: 'Apache License', value: "![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)" },
+              { name: 'Mozilla Public License', value: "![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)" },
+              { name: 'MIT Licenese', value: "![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)"  },
+              { name: 'None', value: ''},]
+              
+           }
     ])
     .then((answers) => {
         
-        const { motivation, build, solved, learn, unique, challenges, future } = answers;
+        const { motivation, build, solved, learn, unique, challenges, future, title, contribution, usage, email, github, licenseChoice } = answers;
         
         const layout = `
-# My Project
+# ${title} 
+
+${licenseChoice} 
 
 ## Motivation
 ${motivation}
+
+## Usage
+${usage}
 
 ## Project Description
 ${build}
@@ -83,8 +131,14 @@ ${unique}
 ## Challenges Faced
 ${challenges}
 
+## Contributors
+${contribution}
+
 ## Future Plans
 ${future}
+
+## Contact
+<a href="${email}" target="_blank">${email}</a> <a href="${github}" target="_blank">GitHub</a>
 `;
 
         writeToFile('README.md', layout);
